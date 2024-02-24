19b6329301700e31127549c691056fda0b43f406
dhruv <dhruvgolani0504@gmail.com>
2024-02-24 17:57:21 +0530

    added logs

--- endpoints/amazon_automation.py
+++ endpoints/amazon_automation.py
@@ -1,7 +1,10 @@
 
 from fastapi import APIRouter
 from crud.AmazonOrdering import AmazonOrdering
+import logging
 
+logging.basicConfig(level=logging.INFO, filename="./logs/amazon.log",filemode="w")
+logger = logging.getLogger("amazon")
 router = APIRouter()
 
 @router.post("/", summary="Initiate Amazon Automation")
@@ -9,6 +12,8 @@ def main(email:str, password:str, product_link:str):
     try:
         amazon_ordering = AmazonOrdering()
         response = amazon_ordering.start_ordering_process_thread(email=email,password=password, product_link=product_link)
+        logger.info("success")
         return response
     except Exception as e:
+        logger.exception("error")
         return e
